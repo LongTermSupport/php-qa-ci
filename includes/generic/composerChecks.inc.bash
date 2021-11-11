a9b7c2d24a94a28b7303c96f7c1a402b5aed1998
Joseph Edmonds <php-book@php-book.local>
2021-11-11 15:08:03 +0000

    bringing in composer normalise

--- includes/generic/composerChecks.inc.bash
+++ includes/generic/composerChecks.inc.bash
@@ -1,7 +1,22 @@
 set +e
+echo "
 
+Checking Composer Issues - Fix Any Red Stuff (wont fail the process)
+---------------------
+"
 phpNoXdebug -f $(which composer) -- diagnose
 
 set -e
+echo "
 
+Running Composer Normalise
+---------------------
+"
+phpNoXdebug -f $(which composer) normalize
+
+echo "
+
+Dumping Composer Autoloader
+---------------------
+"
 phpNoXdebug -f $(which composer) -- dump-autoload
