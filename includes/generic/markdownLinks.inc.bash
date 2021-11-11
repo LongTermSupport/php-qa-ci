95ff27939e91c1d02c89cafe31915ee9b7f35be2
Joseph Edmonds <php-book@php-book.local>
2021-11-11 16:12:53 +0000

    clearer error output on markdown links

--- includes/generic/markdownLinks.inc.bash
+++ includes/generic/markdownLinks.inc.bash
@@ -15,8 +15,8 @@ then
         fi
     done
 else
-    echo "The Markdown Links check requires a README.md in the root of the repository"
-    echo "You must create a README.md to proceed
+    echo "ERROR: The Markdown Links check requires a README.md in the root of the repository"
+    echo "ERROR: You must create a README.md to proceed
     "
     exit 1;
 fi
