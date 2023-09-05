d3d95dc9fc6885e1404d69a3d97b0454bd8591ae
Joseph Edmonds <joseph.edmonds@ballicom.co.uk>
2023-09-05 12:03:54 +0100

    rector support project rector

--- includes/generic/rector.inc.bash
+++ includes/generic/rector.inc.bash
@@ -1,11 +1,34 @@
-rectorExitCode=99
-while ((rectorExitCode > 1)); do
+# First we run the Safe Rectors to implement safe versions of functions.
+rectorSafeExitCode=99
+while ((rectorSafeExitCode > 1)); do
   set +e
+  echo "Running 'Safe' Rector to convert to safe versions of functions"
   phpNoXdebug -f "$binDir"/rector process ${pathsToCheck[@]} \
     --config "$projectRoot/vendor/thecodingmachine/safe/rector-migrate.php"
-  rectorExitCode=$?
+  rectorSafeExitCode=$?
   set -e
-  if ((rectorExitCode > 0)); then
-    tryAgainOrAbort "Rector"
+  if ((rectorSafeExitCode > 0)); then
+    tryAgainOrAbort "Rector 'Safe'"
   fi
 done
+
+# Then we check for project specific Rectors.
+if [[ -f $projectRoot/rector.php ]]; then
+  echo "Running Project Specific Rector as configured in $projectRoot/rector.php"
+  if [[ -f $projectRoot/bin/console ]]; then
+    (cd $projectRoot && APP_ENV=dev phpNoXdebug -f ./bin/console -- cache:clear)
+  fi
+  rectorExitCode=99
+  while ((rectorExitCode > 1)); do
+    set +e
+    echo "Running Project Specific Rector"
+    phpNoXdebug -f "$binDir"/rector process ${pathsToCheck[@]} \
+      --config "$projectRoot/rector.php"
+    rectorExitCode=$?
+    set -e
+    if ((rectorExitCode > 0)); then
+      tryAgainOrAbort "Rector Project Specific"
+    fi
+  done
+fi
+
